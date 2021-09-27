469d4bc9385736fee18a8ed5aae0cefd1aab875d
Laffont Emile <laffonem@f213-09.e-im2ag.ujf-grenoble.fr>
2021-09-27 14:52:25 +0200

    ajout d une fonction pour donner la taille en prennant compte de l alignement

--- src/common.c
+++ src/common.c
@@ -7,8 +7,22 @@
 #define MEMORY_SIZE 4096
 #endif
 
+//allignement
+#if !defined(ALIGNEMENT)
+#define ALIGNEMENT 4
+#endif
+
 static char memory[MEMORY_SIZE];
 
 void *get_memory_adr() { return memory; }
 
 size_t get_memory_size() { return MEMORY_SIZE; }
+
+size_t get_taille_avec_alignement(size_t n){
+	if(n % ALIGNEMENT == 0){
+		return n;
+	}else {
+		return n + (n % ALIGNEMENT);
+	}
+	return n;
+}
